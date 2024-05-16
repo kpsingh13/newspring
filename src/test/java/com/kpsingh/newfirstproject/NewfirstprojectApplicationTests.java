71399a8c6e0e73defee442597d622576632dbf4c
kpsingh13 <kamalpreet2011mms@gmail.com>
2024-05-16 08:02:44 +0530

    first play from home pc

---
+++ src/test/java/com/kpsingh/newfirstproject/NewfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpsingh.newfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
